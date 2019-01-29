85f22b0944cd3129b790d4276741346fe82bbc32
kristjanjansen <kristjan.jansen@gmail.com>
2019-01-29 11:55:14 +0200

    example scenario

---
+++ example/index.js
@@ -0,0 +1,34 @@
+import { Init } from "https://designstem.github.io/fachwerk/mixins.js";
+import * as components from "https://designstem.github.io/fachwerk/components.js";
+import * as utils from "https://designstem.github.io/fachwerk/utils.js";
+
+for (const name in components) {
+  Vue.component(name, components[name]);
+}
+
+new Vue({
+  el: "#app",
+  mixins: [Init],
+  methods: { ...utils },
+  data: { preview: 0, header: 1 },
+  template: `
+  <div>
+    <header v-if="header">
+      Sample content
+    </header>
+    <f-fetch-data url="./index.md">
+      <f-content-editor
+        slot-scope="{ value }"
+        :content="value"
+        :preview="preview"
+        style="--content-editor-min-height: 100vh"
+        save-id="playground"
+      />
+    </f-fetch-data>
+    <f-keyboard alt character="p" @keydown="preview = 1 - preview" />
+    <f-keyboard alt character="h" @keydown="header = 1 - header" />
+    <f-keyboard alt character="left" @keyup="send('prev')" />
+    <f-keyboard alt character="right" @keyup="send('next')" />
+  </div>
+`
+});
---
+++ example/index.md
@@ -0,0 +1,16 @@
+# <big><big>Sample scenario</big></big>
+
+## This is the subheader
+
+> #### Shortcuts
+> Press <kbd>Alt</kbd> <kbd>p</kbd> to switch between editing and preview modes.
+> Press  <kbd>Alt</kbd> <kbd>h</kbd> to hide and show the header.
+> Press <kbd>Alt</kbd> <kbd>←</kbd> and <kbd>Alt</kbd> <kbd>→</kbd> to navigate between slides.
+
+<f-next-button />
+
+---
+
+# Second slide
+
+<f-prev-button />
