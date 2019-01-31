ada459dd9a17f913dd1e00b5b856ad4372be3bb0
Oliver Maaker <maaker@gmail.com>
2019-01-31 11:55:28 +0200

    colorblindness added

---
+++ colorblindness/index.js
@@ -0,0 +1,48 @@
+// Use Javascript module import to get initialization mixin, Vue components and utility functions
+
+import { Init } from "https://designstem.github.io/fachwerk/mixins.js";
+import * as components from "https://designstem.github.io/fachwerk/components.js";
+import * as utils from "https://designstem.github.io/fachwerk/utils.js";
+
+for (const name in components) {
+  Vue.component(name, components[name]);
+}
+
+import ImageCompare from "./components/ImageCompare.js";
+Vue.component('ImageCompare', ImageCompare);
+import ColorblindnessSimulator from "./components/ColorblindnessSimulator.js";
+Vue.component('ColorblindnessSimulator', ColorblindnessSimulator);
+
+new Vue({
+  // Attaching Vue to <div id="app"></div>
+  el: "#app",
+  data: function() {
+    return {
+      before: 'img/before.jpg',
+      after: 'img/after.jpg',
+      padding: {
+        left: 50,
+        right: 50
+      },
+      hideAfter: true
+    }
+  },
+  // Adding a mixin
+  mixins: [Init],
+
+  // Making utilities accessible to templates
+  methods: { ...utils },
+
+  // Fetching the index.md and rendering it
+  template: `                         
+  
+  <f-fetch-data url="./index.md">
+    <f-content 
+      slot-scope="data"
+      :content="data.value"
+    />
+  </f-fetch-data>
+
+
+`
+});
---
+++ colorblindness/index.md
@@ -0,0 +1,27 @@
+
+# <big>Colorblindness</big>
+
+## Interactive components / simulations
+
+<details>
+  <summary>Find out more</summary>
+  <h2>This content was hidden</h2>
+</details>
+
+---
+
+# Image compare
+
+<ImageCompare :before="'images/before.jpg'" :after="'images/after.jpg'" :full="false" :padding ="{left:20, right:20}" :hideAfter="false"></ImageCompare>
+
+<!--<f-next-button />-->
+
+---
+
+# Colorblindness<br />simulator
+
+<ColorblindnessSimulator />
+
+<!--
+<ColorblindnessSimulator :imageUrl="'images/before.jpg'" />
+-->
