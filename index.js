8c034a7d8d5ec4e787c4c42a3379363206a1bd1f
kristjanjansen <kristjan.jansen@gmail.com>
2019-02-13 18:04:01 +0200

    fix index

--- index.js
+++ index.js
@@ -1,26 +1,2 @@
-import { Vue, components, utils } from "https://designstem.github.io/fachwerk/fachwerk.js";
-Vue.prototype.$global = new Vue({ data: { state: {} } });
-
-for (const name in components) {
-  Vue.component(name, components[name]);
-}
-
-new Vue({
-  el: "#app",
-  methods: { ...utils },
-  template: `
-  <div>
-    <header>
-      <div>
-        <a href="https://designstem.github.io/homepage">Home</a> → Scenarios
-      </div>
-    </header>
-    <f-fetch url="./README.md">
-      <f-content
-        slot-scope="{ value }"
-        :content="value"
-      />
-    </f-fetch>
-  </div>
-`
-});
+import { fachwerk } from "https://designstem.github.io/fachwerk/fachwerk.js";
+fachwerk({ src: "./README.md" });
