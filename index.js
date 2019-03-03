2be97806feb5a9f00b21c0c8af2fdc7582780472
kristjanjansen <kristjan.jansen@gmail.com>
2019-03-03 21:15:12 +0000

    cleanup

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import { fachwerk } from "https://designstem.github.io/fachwerk/fachwerk.js";
+
+import FFlower from "./components/FFlower.js"
+import FFlower2 from "./components/FFlower2.js"
+import FClock from "./components/FClock.js"
+import FPlaceholder from "./components/FPlaceholder.js"
+
 fachwerk({
+  components: { FFlower, FFlower2, FClock, FPlaceholder },
   editor: "hide",
   home: false,
   pager: false,
@@ -9,5 +16,6 @@ fachwerk({
     { title: "Project progress", src: "https://designstem.github.io/homepage" },
     { title: "Framework", src: "https://designstem.github.io/homepage" },
     { title: "Github", src: "https://designstem.github.io" }
-  ]
+  ],
+  footer: true
 });
