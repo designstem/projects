df9f69b34972296723f9c39f0c3b39885a957230
matveimug <madis.vaher@gmail.com>
2019-03-04 17:34:54 +0000

    room

--- metamerism/Scene1.js
+++ metamerism/Scene1.js
@@ -3,13 +3,18 @@ import { utils } from "https://designstem.github.io/fachwerk/fachwerk.js";
 export default {
   methods: { ...utils,
     lightProps: function () {
-      return "type: spot; castShadow: true; decay: 0.45; color: "+utils.rgb([0,255][this.r],[0,255][this.g],[0,255][this.b])+")"
+      return "type: point; castShadow: true; decay: 0.45; color: "+utils.rgb([0,255][this.r],[0,255][this.g],[0,255][this.b])+")"
     }
   },
-  data: () => ({ r: 1, g: 1, b: 1 }),
+  data: () => ({
+    r: 1,
+    g: 1,
+    b: 1,
+    frame: 'khaki'
+  }),
   template: `
     <a-scene shadow="type: pcfsoft" debug >'
-      <a-camera>
+      <a-camera position="0 3 0">
         <a-cursor
           fuse="true" 
           fuse-timeout="700"
@@ -17,18 +22,26 @@ export default {
           raycaster="objects: .clickable"
         />
       </a-camera>
-      <a-entity :light=lightProps() position="3 8 7" scale="5 5 5"></a-entity>
-      <a-entity position="0 3 -5" scale="2 3 1">
-        <a-image src="./images/thedress.png" shadow="cast: true" material="shader: standard" />
-        <a-box position="0 -1.6 -0.6" material="color: #ffffff" geometry="depth: 0.1; height: 3; width: 0.1" shadow="cast: true"></a-box>
+      <a-entity :light=lightProps() position="0 8 0" scale="5 5 5"></a-entity>
+      <a-entity position="0 6 -13.9" scale="4 6 1">
+        <a-image src="./images/thedress.png" material="shader: standard" />
+        <a-box position="-0.55 0" material="color: whitesmoke" geometry="depth: 0.2; width: 0.1" shadow=""></a-box>
+        <a-box position="0.55 0" material="color: whitesmoke" geometry="depth: 0.2; width: 0.1" shadow=""></a-box>
+        <a-box position="0 -0.531" material="color: whitesmoke" geometry="depth: 0.2; width: 1.2; height: 0.06" shadow=""></a-box>
+        <a-box position="0 0.531" material="color: whitesmoke" geometry="depth: 0.2; width: 1.2; height: 0.06" shadow=""></a-box>
+        <a-box position="0.8 -0.51 -0.07" material="color: #ffffff" geometry="depth: 0.1; width: .25; height: .1" shadow=""></a-box>
       </a-entity>
-      <a-entity>
-        <a-plane></a-plane>
-        <a-circle id="ground" position="0 0 -4" radius="100" rotation="-90 0 0" shadow="receive: true"></a-circle>
+      <a-entity position="0 0 -4" shadow="receive: true">
+        <a-plane rotation="0 90 0" position="-10 4 0" scale="20 15 15"></a-plane>
+          <a-box rotation="0 90 0" position="-10 4 0" material="color: whitesmoke" geometry="depth: 1; width: 0.1" shadow=""></a-box>
+        <a-plane rotation="0 -90 0" position="10 4 0" scale="20 15 15"></a-plane>
+        <a-plane position="0 4 -10" scale="20 15 15"></a-plane>
+        <a-circle id="ground" radius="100" rotation="-90 0 0"></a-circle>
+        <a-circle id="ceil" radius="100" position="0 11 0" rotation="90 0 0"></a-circle>
       </a-entity>
       
     <a-entity
-      position="0 0.8 -3"
+      position="0 0.8 -2"
       rotation="-20 0 0"
     >
 
