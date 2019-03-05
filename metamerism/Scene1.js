0fa23edc9ba1e8f9d984b3f4a20428b2b6501bd5
matveimug <madis.vaher@gmail.com>
2019-03-05 17:55:14 +0000

    arrow!

--- metamerism/Scene1.js
+++ metamerism/Scene1.js
@@ -58,11 +58,24 @@ export default {
         <a-plane src="#floor" repeat="4 4" rotation="-90 0 0" geometry="height:  20;  width:  20"></a-plane>
         <a-plane rotation="90 0 0" position="0 10 0"  geometry="height:  20;  width:  20"></a-plane>
       </a-entity>
-      <Lamp :light="lightColor" :lampHeight=2.8 position="9 0 -13" />
-      <Lamp :light="lightColor" :lampHeight=2.8 position="9.2 0 -11.9" :lampScale=".6"/>
-      <Lamp :light="lightColor" :lampHeight=2.8 position="-9.3 0 -12.3" :lampScale=".3"/>
-      <Lamp :light="lightColor" :lampHeight=2 position="-8 0 -13.3" :lampScale=".6"/>
-      <Lamp :light="lightColor" :lampHeight=4 position="-9 0 -13" :lampScale=".8" />
+      <a-entity position="9 0 -11">
+      <Lamp :light="lightColor" :lampHeight=2.8 position="0 0 -2" />
+      <Lamp :light="lightColor" :lampHeight=2.8 position="0.2 0 -0.9" :lampScale=".6"/>
+      </a-entity>
+      <a-entity position="-8 0 -12">
+      <Lamp :light="lightColor" :lampHeight=2.8 position="-1.3 0 -0.3" :lampScale=".3"/>
+      <Lamp :light="lightColor" :lampHeight=2 position="0 0 -1.3" :lampScale=".6"/>
+      <Lamp :light="lightColor" :lampHeight=4 position="-1 0 -1" :lampScale=".8" />
+      </a-entity>
+      <a-entity position="9 0 6">
+      <Lamp :light="lightColor" :lampHeight=2.8 position="0 0 -0.9" :lampScale="1.5" />
+      <Lamp :light="lightColor" :lampHeight=2.8 position="0.2 0 -2" :lampScale=".6" />
+      </a-entity>
+      <a-entity position="-8 0 5">
+      <Lamp :light="lightColor" :lampHeight=2.8 position="-0.05 0.1 -1.6" :lampScale=".3"/>
+      <Lamp :light="lightColor" :lampHeight=2 position="1.6 0 0.3" :lampScale=".6"/>
+      <Lamp :light="lightColor" :lampHeight=2.4 position="0.1 0 -0.9" :lampScale="2" />
+      </a-entity>
       <Table position="0 0 -10" rotation="0 -90 0" :tableLength=4.3 :tableWidth=2 :tableHeight=1.4>
         <a-entity scale=".8 .8 .8" rotation="-20 0 0" position="0 0 .3">
           <f-aframe-button @click.native="lightColor = cold" title="cold" position="-1.5 0 0" />
@@ -71,14 +84,19 @@ export default {
         </a-entity>
       </Table>
       <a-entity class="info-board" rotation="0 -80 0" position="3 0 -5">
-        <a-text position="-0.8 3.5 0.05" text="width: 1.6; color: #202020; lineHeight: 60;" 
+        <a-entity position="-.5 4.3 .1" scale=".5 .5 .5">
+          <a-plane material="color: #ccc" height=".1"></a-plane>
+          <a-plane material="color: #ccc" height=".1" width=".6" rotation="0 0 45" position="-.3 .19 0"></a-plane>
+          <a-plane material="color: #ccc" height=".1" width=".6" rotation="0 0 -45" position="-.3 -.19 0"></a-plane>
+        </a-entity>
+        <a-text position="-0.8 3 0.05" text="width: 1.6; color: #202020; lineHeight: 60;" 
         value="The Dress \n 
         In the example of the dress you have seen that the same object may look differently. 
         Some might say it is white and gold, others see it as black and blue. But what is true? 
         To find out more about colors and light you are going to do an experiment in a virtual learning environment. 
         Use a computer or your smartphone (with a VR headset if possible) and enter the first experiment. \n
         1. Make yourself familiar with the VR environment (moving, viewing, controls ...)
-        2. Experiment with the buttons. Which effect do they have?
+        2. Experiment with the buttons on the table. Which effect do they have?
         "></a-text>
         <a-box material="shader: flat" position="0 3 0" geometry="depth: 0.05; width: 2; height: 3.5"></a-box>
         <a-box position="0 1.8 -0.01" geometry="depth: 0.05; width: 2.2; height: 3.5"></a-box>
