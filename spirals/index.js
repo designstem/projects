5f289ae5ff4614f93d86da372b36e181812ec9d7
kristjanjansen <kristjan.jansen@gmail.com>
2019-03-07 10:33:14 +0000

    update

--- spirals/index.js
+++ spirals/index.js
@@ -1,37 +1,3 @@
-import { Vue, components, utils } from "https://designstem.github.io/fachwerk/fachwerk.js";
-Vue.prototype.$global = new Vue({ data: { state: {} } });
+import { fachwerk } from "https://designstem.github.io/fachwerk/fachwerk.js";
 
-for (const name in components) {
-  Vue.component(name, components[name]);
-}
-
-import Spiral from "./Spiral.js";
-Vue.component("Spiral", Spiral);
-
-new Vue({
-
-  el: "#app",
-  data: () => ({
-    dummy: 0,
-  }),
-  computed: {
-    d3Line(){
-      return d3.line().curve(d3.curveCardinal.tension(0));
-    },
-  },
-  methods: { ...utils },
-  template: `
-  <div>
-    <f-fetch url="./README.md">
-      <f-theme slot-scope="data" theme="white">
-        <f-content
-          :content="data.value"
-          :index="0"
-          :base="'1.5vh'"
-          type="slides"
-        />
-      </f-theme>
-    </f-fetch>
-  </div>
-  `
-});
+fachwerk()
