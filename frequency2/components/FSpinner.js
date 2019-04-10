80da78c3b0711709d1c9787687dc89d36752d426
kristjanjansen <kristjan.jansen@gmail.com>
2019-04-10 09:54:13 +0300

    Start working on frequency scenario

---
+++ frequency2/components/FSpinner.js
@@ -0,0 +1,60 @@
+import { Css } from "https://designstem.github.io/fachwerk/fachwerk.js";
+
+export default {
+  mixins: [Css],
+  props: {
+    speed: {
+      default: 1000,
+      type: [Number, String],
+      description: "Rotation speed, in milliseconds"
+    },
+    easing: {
+      default: "linear",
+      type: String,
+      description:
+        "[Easing function](https://developer.mozilla.org/en-US/docs/Web/CSS/animation-timing-function)"
+    },
+    delay: {
+      default: 0,
+      type: [Number, String],
+      description: "Delay in milliseconds"
+    },
+    direction: {
+      default: "normal",
+      type: String,
+      description:
+        "Animation direction: `normal`, `reverse`, `alernate`, `alternate-reverse`"
+    },
+    playing: { default: true, type: Boolean }
+  },
+  computed: {
+    animation() {
+      const animation = [
+        "spinner",
+        `${this.speed}ms`,
+        this.easing,
+        `${this.delay}ms`,
+        "infinite",
+        this.direction,
+        "none",
+        this.playing ? "running" : "paused"
+      ];
+      return animation.join(" ")
+    }
+  },
+  template: `
+  <g :style="{ animation }" class="a">
+    <slot />
+  </g>
+  `,
+  css: `
+  @keyframes spinner {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }  
+  `
+};
---
+++ frequency2/index.md
@@ -0,0 +1,18 @@
+<f-scene grid width="300" height="300">
+  <f-spinner>
+  	<f-box />
+  </f-spinner>
+</f-scene>
+
+---
+
+| theme: yellow
+
+# <big><big>Frequency & Motion</big></big>
+
+<big><big>Learn about the beginnings of moving pictures, the art and the science behind it and why we are still so mesmerized watching those GIF memes.</big></big>
+
+**Skills:** frequency, framerate, sinusoidal wave
+**Tools:** turntable, paper, scissors, pencil(s), printer, smartphone/tablet/PC for online tool
+**Suitable:** indoor, outdoor, group work, individual work
+**Time:** 20min + 15min + 45min (+10 hrs extended exercise)
