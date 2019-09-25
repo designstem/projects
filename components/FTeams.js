ca156715a48f020cf3b070a9651904bf498adf44
Oliver Maaker <maaker@gmail.com>
2019-09-25 12:37:04 -0700

    Merit

--- components/FTeams.js
+++ components/FTeams.js
@@ -1,7 +1,10 @@
+import {Css} from "http://designstem.github.io/fachwerk/fachwerk.js";
+
 export default {
+  mixins: [Css],
   template: `
   <f-sheet id="1-WsazYAKboddKOCkfImHCY6V7Wy-ztIs4qhyYumeAqw" v-slot="{ value: rows }">
-    <div class="grid" style="--cols: repeat(10, 1fr); --gap: 0">
+    <div class="grid f-teams">
       <f-image
         v-for="(r, i) in rows.filter(r => r.shortname)"
         :key="i"
@@ -10,5 +13,17 @@ export default {
       />
     </div>
   </f-sheet>
+  `,
+  css: `
+    .f-teams {
+      display: grid;
+      --cols: repeat(4, 1fr); 
+      --gap: 0;
+    }
+    @media (min-width: 800px) {
+      .f-teams {
+        --cols: repeat(8, 1fr);
+      }
+    }
   `
 };
