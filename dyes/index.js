3617b20a33e8c1b93884f6a7e0c18f4cdc93624a
kristjanjansen <kristjan.jansen@gmail.com>
2019-10-24 18:36:07 +0300

    hiding menu

--- dyes/index.js
+++ dyes/index.js
@@ -1,3 +1,3 @@
 import { fachwerk } from "https://designstem.github.io/fachwerk/fachwerk.js";
 
-fachwerk({ title: 'Dyes', type: "slides", home: "..", menu: "show" });
+fachwerk({ title: 'Dyes', type: "slides", home: ".." });
